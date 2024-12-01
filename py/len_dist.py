4ea345264f940564fa04f5f6b1dfb73d2787281a
terry-u16 <terry-u16@users.noreply.github.com>
2024-12-01 10:20:36 +0900

    w, hの事前分布を調査

---
+++ py/len_dist.py
@@ -0,0 +1,38 @@
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as stats
+
+N = 1000000
+x = []
+
+for i in range(N):
+    U = 100000
+    L = random.randint(U // 10, U // 2)
+    x.append(random.randint(L, U))
+
+x = np.array(x)
+mean = np.mean(x)
+var = np.var(x)
+std = np.std(x)
+skew = stats.skew(x)
+kurtosis = stats.kurtosis(x)
+
+print("mean               :", mean)
+print("variance           :", var)
+print("standard deviation :", std)
+print("skewness           :", skew)
+print("kurtosis           :", kurtosis)
+
+mu, std = stats.norm.fit(x)
+print(mu, std)
+
+plt.hist(x, bins=100, density=True, alpha=0.6)
+
+xmin, xmax = plt.xlim()
+x = np.linspace(xmin, xmax, 100)
+p = stats.norm.pdf(x, mu, std)
+plt.plot(x, p, "k", linewidth=2)
+
+plt.show()
