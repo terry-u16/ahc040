fb5309e5e32cf01c5183a8f845192d0514f77377
terry-u16 <terry-u16@users.noreply.github.com>
2024-12-07 15:39:25 +0900

    ビムサとビムサ+MCTSを交互に実行

--- src/solver/solver01.rs
+++ src/solver/solver01.rs
@@ -66,37 +66,26 @@ impl Solver for Solver01 {
             let remaining_arrange_count = arrange_count - i;
             let duration =
                 (2.9 - input.since().elapsed().as_secs_f64()) / remaining_arrange_count as f64;
-            let beam_duration = duration * 0.5;
-            let mcts_duration = duration * 0.5;
-            let first_step_turn = input.rect_cnt() - 12;
-            let rects = gauss_sampler.sample(&mut rng);
-
-            let ops1 = if use_monte_carlo {
-                beam_arranger.arrange(
-                    &input,
-                    first_step_turn,
-                    rects.clone(),
+            let ops = if i % 2 == 0 {
+                combined_sample(
+                    duration,
+                    input,
+                    &mut gauss_sampler,
                     &mut rng,
-                    beam_duration,
+                    use_monte_carlo,
+                    &mut beam_arranger,
+                    &mut mcts_arranger,
                 )
             } else {
-                beam_arranger.arrange(
-                    &input,
-                    first_step_turn,
-                    rects.clone(),
+                beam_sample(
+                    duration,
+                    input,
+                    &mut gauss_sampler,
                     &mut rng,
-                    beam_duration,
+                    use_monte_carlo,
+                    &mut beam_arranger,
                 )
             };
-
-            let ops2 = if use_monte_carlo {
-                mcts_arranger.arrange(&input, &ops1, rects, &mut rng, mcts_duration)
-            } else {
-                mcts_arranger.arrange(&input, &ops1, rects, &mut rng, mcts_duration)
-            };
-
-            let mut ops = ops1;
-            ops.extend_from_slice(&ops2);
 
             let measure = judge.query(&ops);
 
@@ -106,4 +95,52 @@ impl Solver for Solver01 {
             }
         }
     }
+}
+
+fn beam_sample(
+    duration: f64,
+    input: &Input,
+    gauss_sampler: &mut estimator::gauss::GaussSampler<'_>,
+    rng: &mut rand_pcg::Mcg128Xsl64,
+    use_monte_carlo: bool,
+    beam_arranger: &mut MultiBeamArrangerSimd,
+) -> Vec<crate::problem::Op> {
+    let rects = gauss_sampler.sample(rng);
+
+    if use_monte_carlo {
+        beam_arranger.arrange(&input, input.rect_cnt(), rects.clone(), rng, duration)
+    } else {
+        beam_arranger.arrange(&input, input.rect_cnt(), rects.clone(), rng, duration)
+    }
+}
+
+fn combined_sample(
+    duration: f64,
+    input: &Input,
+    gauss_sampler: &mut estimator::gauss::GaussSampler<'_>,
+    rng: &mut rand_pcg::Mcg128Xsl64,
+    use_monte_carlo: bool,
+    beam_arranger: &mut MultiBeamArrangerSimd,
+    mcts_arranger: &mut MCTSArranger,
+) -> Vec<crate::problem::Op> {
+    let beam_duration = duration * 0.5;
+    let mcts_duration = duration * 0.5;
+    let first_step_turn = input.rect_cnt() - 12;
+    let rects = gauss_sampler.sample(rng);
+
+    let ops1 = if use_monte_carlo {
+        beam_arranger.arrange(&input, first_step_turn, rects.clone(), rng, beam_duration)
+    } else {
+        beam_arranger.arrange(&input, first_step_turn, rects.clone(), rng, beam_duration)
+    };
+
+    let ops2 = if use_monte_carlo {
+        mcts_arranger.arrange(&input, &ops1, rects, rng, mcts_duration)
+    } else {
+        mcts_arranger.arrange(&input, &ops1, rects, rng, mcts_duration)
+    };
+
+    let mut ops = ops1;
+    ops.extend_from_slice(&ops2);
+    ops
 }
